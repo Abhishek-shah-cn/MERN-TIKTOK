a94c7dccf6dc8174a9ca8f5cf0e68b7a4d65170e
abhishek-shah-vst-au4 <54413214+abhishek-shah-vst-au4@users.noreply.github.com>
2020-09-04 11:54:19 +0530

    Fixed Bug for state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import Axios from "axios";
+import React, { useEffect , useState } from "react";
 import Video from "./Video";
+import axios from "./axios";
 
 function App() {
+
+  const [videos, setVideos] = useState([]);
+
+  useEffect (() => {
+    
+    async function fetchPosts() {
+        const response = await Axios.get('/v2/posts')
+        setVideos(response.data);
+
+        return response;
+    }
+
+    fetchPosts();
+  },[])
+
   return (
     <div className="app">
       <h1>This is Mern Staack</h1>
@@ -10,7 +27,8 @@ function App() {
         <Video channel="@bhishek"
         likes={112} shares={458} messages={101} song="Let me Love You" description="Yoyo this is a TikTok"/>
 
-        <Video />
+        <Video channel="@bhishek"
+        likes={2} shares={8} messages={6} song="Dispacito" description="TikTok clone TikTok Clone"/>
       </div>
 
       {/* { app container} */}
